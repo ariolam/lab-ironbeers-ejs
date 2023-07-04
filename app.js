e2113b2295eda2dad6bd3bb5cd4812de544e5ff5
ariola <ariola.markou@gmail.com>
2023-07-04 21:57:33 +0300

    lab solved

--- app.js
+++ app.js
@@ -19,4 +19,15 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  res.render('beers', { beers });
+});
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeers = await punkAPI.getRandom();
+  console.log(randomBeers);
+  res.render('random-beer', randomBeers[0]);
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
